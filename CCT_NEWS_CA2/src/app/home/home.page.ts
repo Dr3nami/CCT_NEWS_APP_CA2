fb474c40d629b9406c0e8a2214fd4552c1ec725f
Dr3nami <you@example.com>
2023-06-28 18:46:29 +0100

    Start Of Html & Css

--- CCT_NEWS_CA2/src/app/home/home.page.ts
+++ CCT_NEWS_CA2/src/app/home/home.page.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { NewsArticlesService } from '../api/news-articles.service';
 
+interface ApiReponse {
+  articles: Article[];
+}
+
+interface Article {
+  
+}
+
 
 @Component({
   selector: 'app-home',
@@ -9,18 +17,18 @@ import { NewsArticlesService } from '../api/news-articles.service';
 })
 export class HomePage {
 selectedCategory ='health'
-topHeadlines = {}
+topHeadlines: Article[] = [];
 
 
   constructor(private articleService:NewsArticlesService) {
-    articleService.getTopHeadline().subscribe((results) =>{
+    articleService.getTopHeadline().subscribe((results: ApiReponse ) =>{
       
-      this.topHeadlines = results.articles
+      this.topHeadlines.push(...results.articles)
       console.log(this.topHeadlines);
       
     })
 
-    articleService.getArticlesByCategory(this.selectedCategory).subscribe((results) =>{
+    articleService.getArticlesByCategory(this.selectedCategory).subscribe((results: ApiReponse) =>{
       console.log((results));
       
     })
